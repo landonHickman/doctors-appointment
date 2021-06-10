6c5ac926aaaafe59f14b18c1003fb299e4e7d84c
landon hickman <landonhickman12@gmail.com>
2021-06-10 15:39:14 -0600

    patients crud works shows doctor and appointment

--- app/controllers/api/patients_controller.rb
+++ app/controllers/api/patients_controller.rb
@@ -1,7 +1,43 @@
 class Api::PatientsController < ApplicationController
+  before_action :set_patient, only: [:show, :destroy, :update]
 
   def index
     render json: Patient.all
   end
 
+  def show
+    render json: @patient.get_doctors_with_appointments
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+    if @patient.save
+      render json: @patient
+    else
+      render json: @patient.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+  
+    if @patient.update(patient_params)
+      render json: @patient
+    else
+      render json: @patient.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    render json: @patient.destroy
+  end
+
+  private
+
+  def set_patient
+    @patient = Patient.find(params[:id])
+  end
+
+  def patient_params
+    params.require(:patient).permit(:name)
+  end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,12 @@
 class Patient < ApplicationRecord
   has_many :appointments, dependent: :destroy
   has_many :doctors, through: :appointments
+
+  def get_doctors_with_appointments
+    appDate = self.doctors.map do |doctor|
+     appointment = doctor.appointments.find_by(patient_id: self.id)
+      {doctor: doctor.name, date: appointment.date}
+    end
+    return {patient: self.name, date: appDate}
+  end
 end
